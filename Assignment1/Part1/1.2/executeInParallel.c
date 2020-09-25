0b2800a3c3fdb6bbb2ac341c5e9cb3c4593d0069
Rohit Ranjan <rohitrjn629@gmail.com>
2020-09-25 19:19:34 +0530

    Add final changes for exPll.c

--- Assignment1/Part1/1.2/executeInParallel.c
+++ Assignment1/Part1/1.2/executeInParallel.c
@@ -15,6 +15,19 @@ int execute_in_parallel(char *infile, char *outfile)
 	if(!paths){
 		exit(-1);
 	}
+	char* path[(int)strlen(paths)];
+	int npath =0;
+	char*token = strtok(paths,":");
+	while(token!= NULL){
+		char tok[(int)strlen(token)+1];
+		strcpy(tok,token);
+		strcat(tok,"/");
+		path[npath] = (char*)malloc((strlen(tok)+1)*sizeof(char));
+		strcpy(path[npath], tok);
+		npath++;
+		token = strtok(NULL,":");
+	}
+	//! path ends
 	//! get commands
 	FILE* fp = fopen(infile, "r");
 	if(!fp){
@@ -27,12 +40,14 @@ int execute_in_parallel(char *infile, char *outfile)
 	while(fgets(str, MX_SZ, fp)){
 		int len = strlen(str);
 		str[len-1] = '\0';
-		cmds[ncmd] = (char*)malloc(len*sizeof(char));
+		cmds[ncmd] = (char*)malloc((len+1)*sizeof(char));
 		strcpy(cmds[ncmd], str);
 		ncmd++;
 	}
+
 	//! end scanning commands
-	pid_t cpid, pid = getpid();
+	pid_t pid = getpid();
+	pid_t cpid;
 	int outfd = open(outfile, O_WRONLY | O_CREAT);
 	if(outfd <0){
 		perror("can't create or open output file");
@@ -45,7 +60,7 @@ int execute_in_parallel(char *infile, char *outfile)
 			perror("pipe can't be processed");
 			exit(-1);
 		}
-		// create argv[] for execv
+		//! create argv[] for execv
 		char*  argv[(int)strlen(cmd) +1];
         int narg =0;
         char *arg = strtok(cmd," ");
@@ -63,36 +78,37 @@ int execute_in_parallel(char *infile, char *outfile)
 			exit(-1);
 		}
 		if(!cpid){
-			char*token = strtok(paths,":");
-			if(dup2(pfd[i][1], 1) <0){
-				perror("can't process dup2");
+			if(dup2(pfd[i][1],1) < 0){
+				perror("Dup2 didn't work");
 				exit(-1);
 			}
-			while(token!= NULL){
-				char tok[(int)strlen(token)+1];
-				strcpy(tok,token);
-				strcat(tok,"/");
-				strcat(tok,argv[0]);
-				execv(tok,argv);
-				token = strtok(NULL,":");
+			for(int i=0;i<npath;i++){
+				char*curr_path = (char*)malloc((strlen(path[i])+1)*sizeof(char));
+				strcpy(curr_path,path[i]);
+				strcat(curr_path, argv[0]);
+				execv(curr_path, argv);
 			}
+			if(write(1, "UNABLE TO EXECUTE\n", 18) <0){
+				perror("Cannot write for invalid commands");
+				exit(-1);
+			}
+			exit(-1);
 		}
 	}
-	// int status;
-	// wait(&status);
-	// for(int i=0;i<2;i++){
-	// 	char buff[4096];
-	// 	ssize_t buffsz = read(pfd[i][0] , buff,4096);
-	// 	if(buffsz < 0 ){
-	// 		perror("Some child processes didn't go well");
-	// 		exit(-1);
-	// 	}
-	// 	buff[buffsz] = '\0';
-	// 	if(write(outfd, buff , buffsz) < 0){
-	// 		perror("Can't write to output file");
-	// 		exit(-1);
-	// 	}
-	// }
+	wait(NULL);
+	for(int i=0;i<ncmd;i++){
+		char buff[4096];
+		ssize_t buffsz = read(pfd[i][0] , buff,4096);
+		if(buffsz < 0 ){
+			perror("Some child processes didn't go well");
+			exit(-1);
+		}
+		buff[buffsz] = '\0';
+		if(write(outfd, buff , buffsz) < 0){
+			perror("Can't write to output file");
+			exit(-1);
+		}
+	}
 	return 0;
 }
 
